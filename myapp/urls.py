f58eaed60cb712effff5725453fc2e8ab9602285
ananda krishnan gr <granandakrishnan@gmail.com>
2024-07-23 06:31:11 +0530

    c

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import RegisterUserAPIView, LoginAPIView, ProtectedExampleView, UserView,LogoutAPIView,CategoryView,ChangePasswordView
 
 urlpatterns = [
+  # This code block is defining URL patterns for a Django application. Each `path` function call maps
+  # a URL pattern to a specific view class. Here's a breakdown of what each line is doing:
     path('register/', RegisterUserAPIView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('transactions/', ProtectedExampleView.as_view(), name='protected'),
